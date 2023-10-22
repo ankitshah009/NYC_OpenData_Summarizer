6edbbe7c8606ed6e91d31b1a88fb37ef9693e367
nkasmanoff <nkasmanoff@gmail.com>
2023-10-22 19:19:17 -0400

    audio

--- server/app.py
+++ server/app.py
@@ -3,6 +3,9 @@ from flask import Flask, request
 from flask_cors import CORS
 import numpy as np
 from rag_generation import answer_nyc_question
+from text_to_voice import generate_audio
+import asyncio
+
 app = Flask(__name__)
 CORS(app)
 
@@ -14,16 +17,19 @@ def home():
 
 @app.route('/about')
 def about():
-    return "This api is used to answer any questions one has about the essential services in NYC".
+    return "This api is used to answer any questions one has about the essential services in NYC"
 
 
-@app.route('/get_answer', methods=['POST']) 
+@app.route('/get_answer', methods=['POST'])
 def get_answer():
     translation_url = "http://burro.mlsp.cs.cmu.edu:5000/translate"
-    question = request.get_json()['question']
-    language = request.get_json()['language']
-    # english langauge key: en
-    translated_response = requests.post(translation_url, data={"q": question, "source": "auto", "target": "en", "format": "text"})
+    # print all request args
+    language = request.form.get('language')
+    question = request.form.get('question')
+    print('question: ', question)
+    print('lang:', language)
+    translated_response = requests.post(translation_url, data={
+                                        "q": question, "source": "auto", "target": "en", "format": "text"})
     translated_question = translated_response.json()['translatedText']
     answer, sources = answer_nyc_question(question)
     payload = {
@@ -35,8 +41,21 @@ def get_answer():
     translated_response = requests.post(translation_url, data=payload)
     translated_answer = translated_response.json()['translatedText']
 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        result = loop.run_until_complete(
+            generate_audio(translated_answer, language))
+    finally:
+        loop.close()
+    return result
+
     return {"answer": answer, "translatedAnswer": translated_answer + '\n\n' + sources}
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# to run the server, run the following command in the terminal:
+# python app.py
--- server/text_to_voice.py
+++ server/text_to_voice.py
@@ -2,31 +2,24 @@ import asyncio
 from msspeech import MSSpeech
 
 
-async def main():
-	mss = MSSpeech()
-	print("Geting voices...")
-	voices = await mss.get_voices_list()
-	print("searching Russian voice...")
-	for voice in voices:
-		if voice["Locale"] == "fr-FR":
-			print("es voice found:", voice["FriendlyName"])
-			await mss.set_voice(voice["Name"])
+async def generate_audio(translated_text, language):
+    language = language.lower() + "-" + language.upper()
+    mss = MSSpeech()
+    print("Geting voices...")
+    voices = await mss.get_voices_list()
+    print("searching Russian voice...")
+    for voice in voices:
+        if voice["Locale"] == language:
+            await mss.set_voice(voice["Name"])
 
-
-	print("*" * 10)
-	filename = "audio.mp3"
-	# with open("s.txt", encoding="UTF8") as f: text:str = f.read()
-	#text = "Или написать текст здесь"
-	#text = "¡Hola! ¿Cómo estás? Si necesitas ayuda con algo específico, no dudes en preguntar"
-	text = "Le petit chat est très mignon. Il a des yeux verts et un pelage doux et soyeux. Il aime jouer avec des balles et des souris en peluche. Il est très affectueux et ronronne souvent. Il dort beaucoup, surtout l’après-midi. Il est très heureux quand il reçoit des caresses et des câlins."
-	print("waiting...")
-	await mss.set_rate(10)
-	await mss.set_pitch(0)
-	await mss.set_volume(1.0)
-	await mss.synthesize(text.strip(), filename)
-	print("*"*10)
-	print("SUCCESS! OK!")
-	print("*"*10)
-
-if __name__ == "__main__":
-	asyncio.run(main())
+    print("*" * 10)
+    filename = "audio.mp3"
+    print("waiting...")
+    await mss.set_rate(10)
+    await mss.set_pitch(0)
+    await mss.set_volume(1.0)
+    await mss.synthesize(translated_text.strip(), filename)
+    print("*"*10)
+    print("SUCCESS! OK!")
+    print("*"*10)
+    print("playing...")
